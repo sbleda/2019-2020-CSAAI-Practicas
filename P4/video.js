3345a8f6e13dd6a8fe8a1e7bd763654099e5ae60
sbleda <60213642+sbleda@users.noreply.github.com>
2020-05-20 14:09:34 +0200

    Add files via upload

--- P4/video.js
+++ P4/video.js
@@ -2,6 +2,10 @@ console.log("Ejecutando JS...");
 
 //----- Obtener elemento de video y configurarlo
 const video = document.getElementById("video")
+const video1 = document.getElementById("video1")
+const video2 = document.getElementById("video2")
+const video3 = document.getElementById("video3")
+
 
 
 //-- Obtener los botones
@@ -30,7 +34,6 @@ video1.play();
 video2.play();
 video3.play();
 
-
 //-- Imagen estática a mostrar cuando el video no
 //-- ha arrancado
 video.poster="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/test.png";
@@ -42,16 +45,29 @@ play1.onclick = () => {
   console.log("Click!");
   video.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4"
   video.play();
+  video1.classList.add("mystyle");
+  video3.classList.remove("mystyle");
+  video2.classList.remove("mystyle");
 };
+
 play2.onclick = () => {
   console.log("Click!");
 
   video.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4"
   video.play();
+  video2.classList.add("mystyle");
+  video1.classList.remove("mystyle");
+  video3.classList.remove("mystyle");
 };
+
 play3.onclick = () => {
   video.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4"
   video.play();
+  video3.classList.add("mystyle");
+  video1.classList.remove("mystyle");
+  video2.classList.remove("mystyle");
+
+
 };
 
 //-- Funcion de retrollamada del boton de parar
@@ -60,4 +76,7 @@ stop.onclick = () => {
   //-- Quitar la fuente de video, para que se muestre la
   //-- imagen definida en el atributo poster
   video.src=null;
+  video3.classList.remove("mystyle");
+  video1.classList.remove("mystyle");
+  video2.classList.remove("mystyle");
 }
